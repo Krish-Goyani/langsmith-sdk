63c9f67818785d1a9f4fa0a362c3dad81cd8a7c8
Krish-Goyani <goyani@duck.com>
2025-05-23 15:42:38 +0530

    fix: async client fixed

--- js/src/utils/warn.ts
+++
@@ -1,8 +0,0 @@
-const warnedMessages: Record<string, boolean> = {};
-
-export function warnOnce(message: string): void {
-  if (!warnedMessages[message]) {
-    console.warn(message);
-    warnedMessages[message] = true;
-  }
-}
